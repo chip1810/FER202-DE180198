bcc38602f87257858402c165b93f5476d4773b34
chip1810 <lechitrung1810@gmail.com>
2025-05-15 11:12:14 +0700

    Add slot1 files

---
+++ slot1/src/Home.js
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <h1>Trang Giới Thiệu</h1>
+      <p>Đây là trang About của ứng dụng React.</p>
+    </div>
+  );
+}
+
+export default About;
